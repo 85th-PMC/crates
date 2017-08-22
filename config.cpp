15ed85164628f2a4e6be6a8af90032e3c6a3b507
Snak3Doc <benjaminbadham@gmail.com>
2017-08-22 19:32:32 +1000

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -68,18 +68,6 @@ class CfgVehicles
 		scope=2;
 		displayName="FirstAid Kit [85th]";
 		model = "\A3\Structures_F_Heli\Items\Luggage\PlasticCase_01_medium_F.p3d";
-		class TransportMagazines{
-			class _xx_HandGrenade {
-				magazine = "HandGrenade";
-				count = 12;
-			};
-		};
-		class TransportWeapons{
-			class _xx_arifle_MX_F {
-				weapon = "arifle_MX_F";
-				count = 2;
-			};
-		};
 		class TransportItems{
 			class _xx_ACE_elasticBandage {
 				name = "ACE_elasticBandage";
@@ -110,12 +98,6 @@ class CfgVehicles
 				count = 8;
 			};
 		};
-		class TransportBackpacks{
-			class _xx_B_Kitbag_mcamo {
-				backpack = "B_Kitbag_mcamo";
-				count = 2;
-			};
-		};
 	};
 };
 class 85th_demolitions_kit: 85th_dummy_crate
@@ -124,27 +106,41 @@ class 85th_demolitions_kit: 85th_dummy_crate
 		displayName="Demolitions Kit [85th]";
 		model = "\A3\Weapons_F\Ammoboxes\Proxy_UsBasicExplosives.p3d";
 		class TransportMagazines{
-			class _xx_HandGrenade {
-				magazine = "HandGrenade";
-				count = 12;
+			class _xx_DemoCharge_Remote_Mag {
+				magazine = "DemoCharge_Remote_Mag";
+				count = 4;
+			};
+		};
+			class _xx_SLAMDirectionalMine_Wire_Mag {
+				magazine = "SLAMDirectionalMine_Wire_Mag";
+				count = 2;
+			};
+		};
+			class _xx_APERSTripMine_Wire_Mag {
+				magazine = "APERSTripMine_Wire_Mag";
+				count = 4;
+			};
+		};
+			class _xx_Claymore_F {
+				magazine = "Claymore_F";
+				count = 2;
 			};
 		};
 		class TransportWeapons{
-			class _xx_arifle_MX_F {
-				weapon = "arifle_MX_F";
-				count = 2;
+			class _xx_ACE_VMM3 {
+				weapon = "ACE_VMM3";
+				count = 1;
 			};
 		};
 		class TransportItems{
-			class _xx_FirstAidKit {
-				name = "FirstAidKit";
-				count = 10;
+			class _xx_ACE_DefusalKit {
+				name = "ACE_DefusalKit";
+				count = 1;
 			};
 		};
-		class TransportBackpacks{
-			class _xx_B_Kitbag_mcamo {
-				backpack = "B_Kitbag_mcamo";
-				count = 2;
+			class _xx_ACE_M26_Clacker {
+				name = "ACE_M26_Clacker";
+				count = 1;
 			};
 		};
 	};
